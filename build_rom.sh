6458a676620e41ef784fab035bee4d53f47da702
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2021-11-09 11:52:51 +0530

    vayu: spark 12: Nuke -device in repo init
    Nuke -device in repo init, as its causing issues

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b spark -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b spark -g default,-mips,-darwin,-notdefault
 git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b saprk_12-vayu .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
