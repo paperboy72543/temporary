77627939d86bf331645a0ee4f1f13ec06af8fa32
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2021-11-12 08:55:23 +0530

    vayu: spark 12: build alpha 1

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b spark -g default,-mips,-darwin,-notdefault
-git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b saprk_12-vayu .repo/local_manifests
+git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b vayu .repo/spark_rom
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
