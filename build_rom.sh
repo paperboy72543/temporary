486c562d1030c3d8581eef61ea2b580175d8d573
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2021-11-12 17:04:53 +0530

    vayu: Spark 12: rebased  manifest repo

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b spark -g default,-mips,-darwin,-notdefault
-git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b vayu .repo/spark_rom
+git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b vayu_spark-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
