0b240a8332cdf252186820d3e3722358eaa477dd
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2021-11-13 21:46:59 +0530

    twolip: PEX 11: EOL

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b ace -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b ace -g default,-mips,-darwin,-notdefault
 git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b PE-11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
-#commit 1
 
 # build rom
 source build/envsetup.sh
 lunch aosp_twolip-userdebug
 export TZ=Asia/Kolkata #put before last build command
-mka bacon -j$(nproc --all)
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
