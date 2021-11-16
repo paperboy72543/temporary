8b27e5f0e4f35fefb7da49b2045ccc47289ba041
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2021-11-16 18:52:32 +0530

    twolip: PEX: initial official build

--- build_rom.sh
+++ build_rom.sh
@@ -9,5 +9,9 @@ lunch aosp_twolip-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
+#OTA Json Generate
+python3 OTA/support/ota.py
+
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy OTA/builds/twolip.json cirrus:twolip -P
