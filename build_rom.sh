9bb87a9e34e97674aab70e9440ec6966dcc327eb
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2021-11-16 21:56:04 +0530

    twolip: PEX 11 official: initial build

--- build_rom.sh
+++ build_rom.sh
@@ -14,6 +14,13 @@ mka bacon
 #OTA Json Generate
 python3 OTA/support/ota.py
 
+#Values for OTA Json File
+Chandler_bing_here
+twolip
+Redmi Note 6 Pro
+https://forum.xda-developers.com/f/xiaomi-redmi-note-6-pro-roms-kernels-recoveries.8410/
+
+
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 rclone copy OTA/builds/twolip.json cirrus:twolip -P
