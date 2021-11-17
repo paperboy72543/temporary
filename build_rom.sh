83e5cacb35307dc3549270ea0cd356e9233748b9
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2021-11-17 11:21:57 +0530

    twolip: PEX official: 2

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b ace -g default,-mips,-darwin,-notdefault
-git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b twolip_aosp-11 .repo/local_manifests
+git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b twolip_pex-11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
